a584fa83fd28145c1b5d9201977ec8701bc3d1a8
deepwwwave <deepwwwave@gmail.com>
2021-12-07 12:41:25 +0100

    ajout responsive

---
+++ client/src/Components/GallerieProduits/GallerieProduits.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./GallerieProduits.css";
+
+export default function GallerieProduits() {
+  return (
+    <div>
+      <div class="gallerie">
+        <div class="container-produit">
+          <div class="container-image-produit">
+            <img src="mohair4.jpeg" alt="" class="image-produit" />
+          </div>
+          <div class="container-titre-prix-produit">
+            <h2 class="titre-produit">Titre du produit</h2>
+            <p>160€</p>
+          </div>
+        </div>
+        <div class="container-produit">
+          <div class="container-image-produit">
+            <img src="mohair4.jpeg" alt="" class="image-produit" />
+          </div>
+          <div class="container-titre-prix-produit">
+            <h2 class="titre-produit">Titre du produit</h2>
+            <p>160€</p>
+          </div>
+        </div>
+        <div class="container-produit">
+          <div class="container-image-produit">
+            <img src="mohair4.jpeg" alt="" class="image-produit" />
+          </div>
+          <div class="container-titre-prix-produit">
+            <h2 class="titre-produit">Titre du produit</h2>
+            <p>160€</p>
+          </div>
+        </div>
+        <div class="container-produit">
+          <div class="container-image-produit">
+            <img src="mohair4.jpeg" alt="" class="image-produit" />
+          </div>
+          <div class="container-titre-prix-produit">
+            <h2 class="titre-produit">Titre du produit</h2>
+            <p>160€</p>
+          </div>
+        </div>
+        <div class="container-produit">
+          <div class="container-image-produit">
+            <img src="mohair4.jpeg" alt="" class="image-produit" />
+          </div>
+          <div class="container-titre-prix-produit">
+            <h2 class="titre-produit">Titre du produit</h2>
+            <p>160€</p>
+          </div>
+        </div>
+        <div class="container-produit">
+          <div class="container-image-produit">
+            <img src="mohair4.jpeg" alt="" class="image-produit" />
+          </div>
+          <div class="container-titre-prix-produit">
+            <h2 class="titre-produit">Titre du produit</h2>
+            <p>160€</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/Containers/Boutique/Boutique.js
+++ client/src/Containers/Boutique/Boutique.js
@@ -1,12 +1,14 @@
 import './Boutique.css'
 import BoutiqueMenu from '../../Components/BoutiqueMenu/BoutiqueMenu'
 import PanierLogo from '../../Components/Panier/PanierLogo'
+import GallerieProduits from '../../Components/GallerieProduits/GallerieProduits'
 
 export default function Boutique() {
     return (
         <div>
             <PanierLogo/>
             <BoutiqueMenu/>
+            <GallerieProduits/>
         </div>
     )
 }
